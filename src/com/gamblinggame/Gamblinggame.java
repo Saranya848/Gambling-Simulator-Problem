38e176053135fa209c0e462a8fe42cf17601c97e
SaranyaSiripurapu <Saranyasripurapu@gmail.com>
2022-02-19 08:45:23 +0530

    Print stake and bet

---
+++ src/com/gamblinggame/Gamblinggame.java
@@ -0,0 +1,18 @@
+package com.gamblinggame;
+import java.util.*;
+public class Gamblinggame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner g = new Scanner(System.in);
+		
+		System.out.println("Enter stack of every day");
+		int stack = g.nextInt();
+		System.out.println("Enter bet of every day");
+
+		int bet = g.nextInt();
+		g.close();
+	}
+	
+
+}
